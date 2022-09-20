cec68f8b5d700e87d635a5105e1bfab5531dd5a8
Unekwe Francis <unekwenchekwube@gmail.com>
2022-09-20 10:04:16 +0100

    added new colors to palete

--- src/constants/pallete.ts
+++ src/constants/pallete.ts
@@ -3,6 +3,10 @@ const pallete = {
   white: '#fff',
   black: '#000',
   lightGray: '#D9D9D9',
+  error: 'rgb(255,0,0)',
+  bgTextInput: '#ccc',
+  gray: ' #ccc',
+  lightGreen: '#5BD6A6',
 };
 
 export default pallete;
