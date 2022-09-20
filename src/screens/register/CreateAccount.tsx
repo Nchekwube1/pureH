89496037ff5520b443067399a350bf42c8609fbf
Unekwe Francis <unekwenchekwube@gmail.com>
2022-09-20 10:04:34 +0100

    create account screen

--- src/screens/register/CreateAccount.tsx
+++ src/screens/register/CreateAccount.tsx
@@ -1,10 +1,24 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
+import React, {useState} from 'react';
+import Header from '../../components/header/Header';
+import globalStyles from '../../styles/globasStyles';
+import TextInputComponent from '../../components/textinput/TextInput';
 
 const CreateAccount = () => {
+  const [fullName, setFullName] = useState('');
   return (
-    <View>
-      <Text>CreateAccount</Text>
+    <View style={[globalStyles.px2]}>
+      <Header />
+      <TextInputComponent
+        value={fullName}
+        placeholder="Enter password"
+        onChangeText={val => setFullName(val)}
+      />
+      <TextInputComponent
+        value={fullName}
+        placeholder="Enter email"
+        onChangeText={val => setFullName(val)}
+      />
     </View>
   );
 };
